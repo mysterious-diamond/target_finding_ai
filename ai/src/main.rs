0240a7a70dd6b376601cc064095516c13475cdec
mysterious-diamond <aaronkenn10@gmail.com>
2026-06-09 15:15:33 +0700

    Finished Project

--- ai/src/main.rs
+++ ai/src/main.rs
@@ -1,6 +1,206 @@
-use std::error::Error;
-
+use bytemuck::cast_slice;
+use rand_distr::{Distribution, Normal};
+use std::{error::Error, io};
 use tokio::net::windows::named_pipe::{NamedPipeServer, ServerOptions};
+
+struct AI {
+    first_layer_weights: Vec<f32>,
+    first_layer_biases: Vec<f32>,
+    second_layer_weights: Vec<f32>,
+    second_layer_biases: Vec<f32>,
+    output_layer_weights: Vec<f32>,
+    output_layer_biases: Vec<f32>,
+    hidden_layer_neurons: usize,
+    standard_deviation: f32,
+    learning_rate: f32,
+}
+
+impl AI {
+    pub fn new() -> AI {
+        let hidden_layer_neurons: usize = 32;
+        let mut first_layer_weights: Vec<f32> = vec![0.0; 2 * hidden_layer_neurons];
+        let mut second_layer_weights: Vec<f32> =
+            vec![0.0; hidden_layer_neurons * hidden_layer_neurons];
+        let mut output_layer_weights: Vec<f32> = vec![0.0; hidden_layer_neurons * 2];
+
+        let first_layer_biases: Vec<f32> = vec![0.0; hidden_layer_neurons];
+        let second_layer_biases: Vec<f32> = vec![0.0; hidden_layer_neurons];
+        let output_layer_biases: Vec<f32> = vec![0.0; 2];
+
+        let first_layer_distribution = Normal::new(0.0, 1.0f32.sqrt()).unwrap();
+        let second_layer_distribution =
+            Normal::new(0.0, (2.0 / hidden_layer_neurons as f32).sqrt()).unwrap();
+        let output_layer_distribution =
+            Normal::new(0.0, (1.0 / hidden_layer_neurons as f32).sqrt()).unwrap();
+
+        for i in 0..hidden_layer_neurons {
+            for j in 0..2 {
+                first_layer_weights[i * 2 + j] = first_layer_distribution.sample(&mut rand::rng());
+            }
+        }
+
+        for i in 0..hidden_layer_neurons {
+            for j in 0..hidden_layer_neurons {
+                second_layer_weights[i * hidden_layer_neurons + j] =
+                    second_layer_distribution.sample(&mut rand::rng());
+            }
+        }
+
+        for i in 0..2 {
+            for j in 0..hidden_layer_neurons {
+                output_layer_weights[i * hidden_layer_neurons + j] =
+                    output_layer_distribution.sample(&mut rand::rng());
+            }
+        }
+
+        AI {
+            first_layer_weights,
+            first_layer_biases,
+            second_layer_weights,
+            second_layer_biases,
+            output_layer_weights,
+            output_layer_biases,
+            hidden_layer_neurons,
+            standard_deviation: 0.5,
+            learning_rate: 0.005,
+        }
+    }
+
+    pub fn forward(&self, inputs: &[f32]) -> (Vec<f32>, Vec<f32>, Vec<f32>, Vec<f32>) {
+        let mut first_layer_activations: Vec<f32> = vec![0.0; self.hidden_layer_neurons];
+
+        for (i, bias) in self.first_layer_biases.iter().enumerate() {
+            let mut sum: f32 = *bias;
+            for (j, input) in inputs.iter().enumerate() {
+                sum += self.first_layer_weights[i * 2 + j] * input;
+            }
+
+            first_layer_activations[i] = if sum > 0.0 { sum } else { 0.0 };
+        }
+
+        let mut second_layer_activations: Vec<f32> = vec![0.0; self.hidden_layer_neurons];
+
+        for (i, bias) in self.second_layer_biases.iter().enumerate() {
+            let mut sum: f32 = *bias;
+            for (j, activation) in first_layer_activations.iter().enumerate() {
+                sum += self.second_layer_weights[i * self.hidden_layer_neurons + j] * activation;
+            }
+
+            second_layer_activations[i] = if sum > 0.0 { sum } else { 0.0 };
+        }
+
+        let mut output_layer_activations: Vec<f32> = vec![0.0; 2];
+        for (i, bias) in self.output_layer_biases.iter().enumerate() {
+            let mut sum: f32 = *bias;
+            for (j, activation) in second_layer_activations.iter().enumerate() {
+                sum += self.output_layer_weights[i * self.hidden_layer_neurons + j] * activation;
+            }
+
+            output_layer_activations[i] = sum.tanh();
+        }
+
+        let mut outputs: Vec<f32> = vec![0.0; 2];
+        for (i, output) in outputs.iter_mut().enumerate() {
+            let distribution =
+                Normal::new(output_layer_activations[i], self.standard_deviation).unwrap();
+            *output = distribution.sample(&mut rand::rng());
+        }
+
+        (
+            first_layer_activations,
+            second_layer_activations,
+            output_layer_activations,
+            outputs,
+        )
+    }
+
+    pub fn train(
+        &mut self,
+        first_layer_activations: &[f32],
+        second_layer_activations: &[f32],
+        output_layer_activations: &[f32],
+        inputs: &[f32],
+        outputs: &[f32],
+        score: f32,
+    ) {
+        let mut second_layer_weights: Vec<f32> = vec![0.0; self.hidden_layer_neurons.pow(2)];
+        let mut second_layer_biases: Vec<f32> = vec![0.0; self.hidden_layer_neurons];
+
+        let mut output_layer_weights: Vec<f32> = vec![0.0; self.hidden_layer_neurons * 2];
+        let mut output_layer_biases: Vec<f32> = vec![0.0; 2];
+
+        let mut output_layer_loss: Vec<f32> = vec![0.0; 2];
+        for i in 0..2 {
+            output_layer_loss[i] = (-(outputs[i] - output_layer_activations[i]) * score
+                / self.standard_deviation.powf(2.0))
+            .clamp(-5.0, 5.0);
+
+            output_layer_loss[i] *= 1.0 - output_layer_activations[i].powf(2.0);
+        }
+
+        for (i, loss) in output_layer_loss.iter().enumerate() {
+            for j in 0..self.hidden_layer_neurons {
+                output_layer_weights[i * self.hidden_layer_neurons + j] = self.output_layer_weights
+                    [i * self.hidden_layer_neurons + j]
+                    - loss * second_layer_activations[j] * self.learning_rate;
+            }
+
+            output_layer_biases[i] = self.output_layer_biases[i] - loss * self.learning_rate;
+        }
+
+        let mut second_layer_loss: Vec<f32> = vec![0.0; self.hidden_layer_neurons];
+        for (j, loss) in second_layer_loss.iter_mut().enumerate() {
+            for (i, output_loss) in output_layer_loss.iter().enumerate() {
+                *loss += self.output_layer_weights[i * self.hidden_layer_neurons + j] * output_loss;
+            }
+
+            *loss *= if second_layer_activations[j] > 0.0 {
+                1.0
+            } else {
+                0.0
+            };
+        }
+
+        for (i, loss) in second_layer_loss.iter().enumerate() {
+            for j in 0..self.hidden_layer_neurons {
+                second_layer_weights[i * self.hidden_layer_neurons + j] = self.second_layer_weights
+                    [i * self.hidden_layer_neurons + j]
+                    - loss * first_layer_activations[j] * self.learning_rate;
+            }
+            second_layer_biases[i] = self.second_layer_biases[i] - loss * self.learning_rate;
+        }
+
+        let mut first_layer_loss: Vec<f32> = vec![0.0; self.hidden_layer_neurons];
+        for (j, loss) in first_layer_loss.iter_mut().enumerate() {
+            for (i, second_loss) in second_layer_loss.iter().enumerate() {
+                *loss += self.second_layer_weights[i * self.hidden_layer_neurons + j] * second_loss;
+            }
+
+            *loss *= if first_layer_activations[j] > 0.0 {
+                1.0
+            } else {
+                0.0
+            };
+        }
+
+        for (i, loss) in first_layer_loss.iter().enumerate() {
+            for (j, input) in inputs.iter().enumerate() {
+                self.first_layer_weights[i * 2 + j] -= loss * input * self.learning_rate;
+            }
+
+            self.first_layer_biases[i] -= loss * self.learning_rate;
+        }
+
+        self.second_layer_weights = second_layer_weights;
+        self.second_layer_biases = second_layer_biases;
+        self.output_layer_weights = output_layer_weights;
+        self.output_layer_biases = output_layer_biases;
+        if score == 10000.0 {
+            self.standard_deviation *= 0.9995;
+        }
+        self.standard_deviation = self.standard_deviation.max(0.2);
+    }
+}
 
 async fn await_pipe_connection() -> Result<NamedPipeServer, Box<dyn Error>> {
     let pipe: NamedPipeServer = ServerOptions::new().create(r"\\.\pipe\target-ai")?;
@@ -12,13 +212,47 @@ async fn await_pipe_connection() -> Result<NamedPipeServer, Box<dyn Error>> {
     Ok(pipe)
 }
 
-#[tokio::main]
-async fn main() {
-    let pipe: NamedPipeServer = await_pipe_connection()
-        .await
-        .expect("Couldn't connect pipe...");
+async fn write_data(pipe: &NamedPipeServer, data: &[f32]) {
+    loop {
+        pipe.writable()
+            .await
+            .expect("Error while waiting for pipe to be readable");
 
-    let mut buffer = vec![0; 2 * 4];
+        match pipe.try_write(cast_slice(data)) {
+            Ok(_) => break,
+            Err(err) if err.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(err) => panic!("Error while trying to write, {}", err),
+        }
+    }
+}
+
+async fn wait_for_score_message(pipe: &NamedPipeServer) -> f32 {
+    let mut buf = vec![0; 4];
+    loop {
+        pipe.readable()
+            .await
+            .expect("Error while waiting for score message");
+
+        match pipe.try_read(&mut buf) {
+            Ok(0) => return 0.0,
+            Ok(n) => {
+                let data: Vec<f32> = buf[..n]
+                    .chunks_exact(4)
+                    .map(|chunk| {
+                        let array: [u8; 4] = chunk.try_into().expect("Data broken or corrupted");
+                        f32::from_le_bytes(array)
+                    })
+                    .collect();
+
+                return data[0];
+            }
+            Err(err) if err.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(err) => panic!("Error while reading score {}", err),
+        }
+    }
+}
+
+async fn listener_loop(pipe: &NamedPipeServer, mut ai: AI, mut buffer: Vec<u8>) {
     loop {
         pipe.readable()
             .await
@@ -27,14 +261,29 @@ async fn main() {
         match pipe.try_read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => {
-                println!("Received {} bytes", n);
-                let _data: Vec<f32> = buffer[..n]
+                let data: Vec<f32> = buffer[..n]
                     .chunks_exact(4)
                     .map(|chunk| {
                         let array: [u8; 4] = chunk.try_into().expect("Data broken or corrupted");
                         f32::from_le_bytes(array)
                     })
                     .collect();
+                let (
+                    first_layer_activations,
+                    second_layer_activations,
+                    output_layer_activations,
+                    outputs,
+                ) = ai.forward(&data);
+                write_data(pipe, &outputs).await;
+                let score: f32 = wait_for_score_message(pipe).await;
+                ai.train(
+                    &first_layer_activations,
+                    &second_layer_activations,
+                    &output_layer_activations,
+                    &data,
+                    &outputs,
+                    score,
+                );
             }
             Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => continue,
             Err(err) if err.kind() == std::io::ErrorKind::BrokenPipe => break,
@@ -46,4 +295,16 @@ async fn main() {
             }
         }
     }
+}
+
+#[tokio::main]
+async fn main() {
+    let pipe: NamedPipeServer = await_pipe_connection()
+        .await
+        .expect("Couldn't connect pipe...");
+
+    let ai: AI = AI::new();
+
+    let buffer = vec![0; 2 * 4];
+    listener_loop(&pipe, ai, buffer).await;
 }
--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -1,4 +1,7 @@
+#include <algorithm>
+#include <array>
 #include <cmath>
+#include <format>
 #include <iostream>
 #include <random>
 
@@ -15,7 +18,8 @@
 #define SCREEN_WIDTH 1000
 #define PLAYERSIZE 30
 #define TARGET_SIZE 30
-#define WIN_SCORE 1000
+#define WIN_SCORE 10000
+#define MAX_SPEED 5.0f
 
 class Target {
    public:
@@ -35,8 +39,10 @@ class Target {
 Target::Target(float targetSize) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> distrib_x(0, SCREEN_WIDTH);
-    std::uniform_real_distribution<float> distrib_y(0, SCREEN_HEIGHT);
+    std::uniform_real_distribution<float> distrib_x(
+        targetSize / 2, SCREEN_WIDTH - targetSize / 2);
+    std::uniform_real_distribution<float> distrib_y(
+        targetSize / 2, SCREEN_HEIGHT - targetSize / 2);
 
     this->pos = {distrib_x(gen), distrib_y(gen)};
     this->targetSize = targetSize;
@@ -51,7 +57,8 @@ void Target::draw() {
 }
 
 void Target::place() {
-    this->pos = {this->distrib_x(this->gen), this->distrib_y(this->gen)};
+    this->pos = {this->distrib_x(this->gen) - this->targetSize / 2,
+                 this->distrib_y(this->gen) - this->targetSize / 2};
 }
 
 Vector2 Target::getPos() { return this->pos; }
@@ -62,30 +69,45 @@ class Player {
     void move(float dx, float dy);
     void draw();
     float getScore(Vector2 targetPos, float targetSize);
+    Vector2 getDistance(Vector2 targetPos, float targetSize);
 
    private:
     Vector2 curPos;
     Vector2 lastPos;
     float playerSize;
+    double timeSinceLastTarget;
 };
 
 Player::Player(float x, float y, float playerSize) {
     this->curPos = {x, y};
     this->lastPos = this->curPos;
     this->playerSize = playerSize;
+    this->timeSinceLastTarget = 0.0;
 }
 
 void Player::move(float dx, float dy) {
     this->lastPos = curPos;
-    this->curPos = {this->curPos.x + dx, this->curPos.y + dy};
+    float newX = this->curPos.x + this->playerSize / 2 + dx;
+    float newY = this->curPos.y + this->playerSize / 2 + dy;
+    this->curPos = {newX - this->playerSize / 2, newY - this->playerSize / 2};
 }
 
 void Player::draw() {
-    DrawRectangle(this->curPos.x, this->curPos.y, this->playerSize,
-                  this->playerSize, RED);
+    DrawRectangle(
+        std::clamp(this->curPos.x + this->playerSize / 2, this->playerSize / 2,
+                   (float)SCREEN_WIDTH - this->playerSize / 2) -
+            this->playerSize / 2,
+        std::clamp(this->curPos.y + this->playerSize / 2, this->playerSize / 2,
+                   (float)SCREEN_HEIGHT - this->playerSize / 2) -
+            this->playerSize / 2,
+        this->playerSize, this->playerSize, RED);
 }
 
 float Player::getScore(Vector2 targetPos, float targetSize) {
+    if (GetTime() - timeSinceLastTarget > 3.0) {
+        this->timeSinceLastTarget = GetTime();
+        return -100.0f;
+    }
     float currentDistance =
         std::sqrt(std::pow((this->curPos.x + this->playerSize / 2) -
                                (targetPos.x + targetSize / 2),
@@ -93,7 +115,10 @@ float Player::getScore(Vector2 targetPos, float targetSize) {
                   std::pow((this->curPos.y + this->playerSize / 2) -
                                (targetPos.y + targetSize / 2),
                            2));
-    if (currentDistance <= this->playerSize) return WIN_SCORE;
+    if (currentDistance <= this->playerSize / 2 + targetSize / 2) {
+        this->timeSinceLastTarget = GetTime();
+        return WIN_SCORE;
+    }
 
     float lastDistance =
         std::sqrt(std::pow((this->lastPos.x + this->playerSize / 2) -
@@ -103,7 +128,17 @@ float Player::getScore(Vector2 targetPos, float targetSize) {
                                (targetPos.y + targetSize / 2),
                            2));
     float difference = lastDistance - currentDistance;
-    return difference * 10;
+    return difference * 20 - 1.0f;
+}
+
+Vector2 Player::getDistance(Vector2 targetPos, float targetSize) {
+    float dx = (this->curPos.x + this->playerSize / 2) -
+               (targetPos.x + targetSize / 2);
+
+    float dy = (this->curPos.y + this->playerSize / 2) -
+               (targetPos.y + targetSize / 2);
+
+    return {dx, dy};
 }
 
 HANDLE establish_pipe_connection() {
@@ -122,12 +157,31 @@ HANDLE establish_pipe_connection() {
     return pipe;
 }
 
+std::array<float, 2> handle_ai_step(HANDLE pipe, Vector2 dist) {
+    DWORD bytes_written;
+    float data[2] = {dist.x / (float)SCREEN_WIDTH,
+                     dist.y / (float)SCREEN_HEIGHT};
+    WriteFile(pipe, data, sizeof(float) * 2, &bytes_written, NULL);
+
+    float result[2] = {};
+    DWORD bytes_read;
+    bool _ = ReadFile(pipe, result, sizeof(float) * 2, &bytes_read, NULL);
+
+    return {result[0], result[1]};
+}
+
 int main() {
+    bool isManual = false;
+    int wins = 0;
+    std::cout << "Are you training AI or are you manually playing? (0 for "
+                 "training, 1 for manual)\n";
+    std::cin >> isManual;
     Player player =
         Player((float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2, PLAYERSIZE);
 
     Target target = Target(TARGET_SIZE);
-    HANDLE pipe = establish_pipe_connection();
+    HANDLE pipe;
+    if (!isManual) pipe = establish_pipe_connection();
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Path Finding AI");
     SetTargetFPS(60);
@@ -135,24 +189,47 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(GRAY);
+        DrawText(std::format("You have {} wins!", wins).c_str(),
+                 SCREEN_WIDTH / 2 -
+                     MeasureText(std::format("You have {} wins!", wins).c_str(),
+                                 40) /
+                         2,
+                 50, 40, WHITE);
+
         player.draw();
         target.draw();
 
-        if (IsKeyDown(KEY_W)) {
+        if (IsKeyDown(KEY_W) && isManual) {
             player.move(0, -200 * GetFrameTime());
         }
-        if (IsKeyDown(KEY_S)) {
+        if (IsKeyDown(KEY_S) && isManual) {
             player.move(0, 200 * GetFrameTime());
         }
-        if (IsKeyDown(KEY_A)) {
+        if (IsKeyDown(KEY_A) && isManual) {
             player.move(-200 * GetFrameTime(), 0);
         }
-        if (IsKeyDown(KEY_D)) {
+        if (IsKeyDown(KEY_D) && isManual) {
             player.move(200 * GetFrameTime(), 0);
         }
 
+        if (!isManual) {
+            Vector2 dist = player.getDistance(target.getPos(), TARGET_SIZE);
+            std::array<float, 2> result = handle_ai_step(pipe, dist);
+            player.move(result[0] * MAX_SPEED, result[1] * MAX_SPEED);
+        }
         float score = player.getScore(target.getPos(), TARGET_SIZE);
-        if (score == WIN_SCORE) target.place();
+
+        if (!isManual) {
+            DWORD bytes_written;
+            WriteFile(pipe, &score, sizeof(float), &bytes_written, NULL);
+        }
+
+        if (score == WIN_SCORE) {
+            wins++;
+            target.place();
+        } else if (score == -100.0f) {
+            target.place();
+        }
 
         EndDrawing();
     }
